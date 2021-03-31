f2b000cfac7eb2dfad15dab78e762a4b87e14c88
famouzkk <famouzkk@gmail.com>
2021-03-31 19:39:37 +0200

    saves system

--- src/PlayerConfig.cpp
+++ src/PlayerConfig.cpp
@@ -1,20 +1,30 @@
 #include "PlayerConfig.hpp"
 #include <fstream>
 
-PlayerConfig::PlayerConfig() : m_exists(false) {
+PlayerConfig::PlayerConfig() : m_hasPlayed(false) {
 
 }
 
-PlayerConfig::PlayerConfig(const std::filesystem::path& path) : m_exists(false) {
-    loadConfig(path);
+PlayerConfig::PlayerConfig(const std::filesystem::path& path) : m_hasPlayed(false) {
+    if(!loadConfig(path)) {
+        m_playerConfigJson = R"(
+        {
+            "player_name" : "Adam",
+            "score" : 0,
+            "last_map_played" : {
+                    "path" : "none",
+                    "logic_grid" : []
+                }
+        })"_json;
+    }
 }
 
 PlayerConfig:: ~PlayerConfig() {
 
 }
 
-bool PlayerConfig::exists() const {
-    return m_exists;
+bool PlayerConfig::hasPlayed() const {
+    return m_hasPlayed;
 }
 
 bool PlayerConfig::loadConfig(const std::filesystem::path& path) {
@@ -35,18 +45,17 @@ bool PlayerConfig::loadConfig(const std::filesystem::path& path) {
         //can throw parsing error, thats why we are using try catch
         m_playerConfigJson = json::parse(m_playerConfigStream);
         m_jsonPath = path;
-        m_exists = true;
+        m_hasPlayed = true;
     } 
     catch(std::exception &e) {
-        m_exists = false;
+        m_hasPlayed = false;
         return false;
     }
-
     return true;
 }
 
 std::filesystem::path PlayerConfig::getLastPlayedLevelPath() const {
-    return "../res/levels/official/" + m_playerConfigJson.at("last_map_played").at("path").get<std::string>();
+    return "../" + m_playerConfigJson.at("last_map_played").at("path").get<std::string>();
 }
 
 //returns position of boxes and player on last played map
@@ -54,6 +63,22 @@ std::vector<int> PlayerConfig::getSavedLogicGrid() const {
     return m_playerConfigJson.at("last_map_played").at("logic_grid");
 }
 
-bool PlayerConfig::saveConfig(const std::string &name, const int score) {
+void PlayerConfig::setScore(const int score) {
+    m_playerConfigJson.at("score") = score;
+}
+
+void PlayerConfig::setLogicGrid(std::vector<int> logicGrid) {
+    m_playerConfigJson.at("last_map_played").at("logic_grid") = logicGrid;
+}
+
+void PlayerConfig::saveConfig(const std::string &playerName, const std::filesystem::path& levelConfigPath) {
+    std::filesystem::path savePath = "../res/saves/" + playerName + ".json";
     
+    m_playerConfigJson.at("player_name") = playerName;
+    m_playerConfigJson.at("last_map_played").at("path") = levelConfigPath;
+  
+    std::ofstream saveStream(savePath);
+    saveStream << m_playerConfigJson;
+    saveStream.close();
+    m_jsonPath = savePath;
 }
